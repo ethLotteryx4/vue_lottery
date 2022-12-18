e2e565cbfefeb40391adce0260557b3933ac0c36
chuanmx20 <chuanmx20@mails.tsinghua.edu.cn>
2022-12-18 17:54:50 +0800

    [stage] add login func

--- src/components/store.vue
+++ src/components/store.vue
@@ -5,13 +5,14 @@
             <div class="vstack">
                 <div class="hstack">
                     <span class="txt">钱包：</span>
-                <input id="acc" ref="acc" placeholder="wallet-address"/>
+                <input id="acc" ref="acc" placeholder="wallet-address" v-model="acc"/>
                 </div>
                 <div class="hstack">
                     <span class="txt">私钥：</span>
-                <input id="pk" ref="pk" placeholder="private-key" />
+                <input id="pk" ref="pk" placeholder="private-key" v-model="pk" />
                 </div>
             </div>
+            <button class="login" @click="login">登录</button>
             <button class="buy" @click="submit">购买</button>
         </div>
     </div>
@@ -20,7 +21,7 @@
 <script>
 /* eslint-disable */
 import Form from '@/components/Form'
-import {buy} from '../utils.js'
+import * as utils from '../utils.js'
 export default {
     name: 'Store',
     components: {
@@ -28,29 +29,52 @@ export default {
     },
     data () {
         return {
-            rows: []
+            rows: [],
+            data: [],
+            acc: "",
+            pk: ""
         }
     },
     methods: {
+        login() {
+            // TODO:
+            if (!utils.is_address(acc)) {
+                alert("请输入正确的钱包地址！")
+                return;
+            }
+        },
         submit() {
-            var data = this.$refs.form.form_data()
-            var acc = this.$refs.acc.value
-            var pk = this.$refs.pk.value
-            if (buy(data, pk, acc)) {
+            if (!utils.is_address(acc)) {
+                alert("请输入正确的钱包地址！")
+                return;
+            }
+            if (utils.buy(this.data, this.pk, this.acc)) {
                 alert("购买成功！")
             } else {
-                alert("")
+                alert("购买失败")
             }
         }
     }
 }
 </script>
 <style>
+button.login {
+    background-color: #FFCCCC;
+        margin:15px 0px 20px 20px;
+        width: 100px;
+        height: 100px;
+        font-size: 125%;
+        font-weight: 600;
+        color: #7a366a;
+}
 button.buy {
     background-color: #FFCCCC;
         margin:15px 0px 20px 20px;
         width: 100px;
         height: 100px;
+        font-size: 125%;
+        font-weight: 600;
+        color: #7a366a;
 }
 div.hstack {
     flex-direction:row;
